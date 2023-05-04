fac0b917648aaf8f2f5fb9bf0b90d71a0315cdad
Garikai <52905358+gfundire@users.noreply.github.com>
2023-05-04 14:33:32 +0000

    updates App.css,App.js,App.test.js and index.js

--- little-lemon/src/App.js
+++ little-lemon/src/App.js
@@ -1,24 +1,129 @@
-
 import './App.css';
 
-function App() {
+import { useState, useReducer } from 'react';
+import { Routes, Route } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
+
+import Nav from './components/Nav';
+import Home from './components/Home';
+import About from './components/About';
+import BookingForm from './components/BookingForm';
+import DetailsForm from './components/DetailsForm';
+import ConfirmedBooking from './components/ConfirmedBooking';
+import Orders from './components/Orders';
+import Error404 from './components/Error404';
+import Login from './components/Login';
+import Footer from './components/Footer';
+
+import useBooking from './hooks/useBookings';
+import { fetchAPI, submitAPI } from './utils/api';
+
+export default function App() {
+  const navigate = useNavigate();
+  const { formik } = useBooking();
+
+  const initialFormValues = {
+    res_date: '',
+    res_time: '',
+    guests: '',
+    occasion: '',
+    seatingOption: '',
+  };
+
+  const [bookingData, setBookingData] = useState(initialFormValues);
+
+  const initialTimes = ['17:00', '18:00', '19:00', '20:00', '21:00', '22:00'];
+
+  const updateTimes = (state, action) => {
+    if (action.type === 'change_date') {
+      const date = new Date(action.date);
+      return fetchAPI(date);
+    }
+    return state;
+  };
+  const initializeTimes = (times) => times;
+  const [availableTimes, dispatch] = useReducer(
+    updateTimes,
+    initialTimes,
+    initializeTimes
+  );
+
+  const handleDateChange = (e) => {
+    dispatch({ type: 'change_date', date: e.target.value });
+    setBookingData({ ...bookingData, [e.target.name]: e.target.value });
+  };
+
+  const handleBookingData = (e) => {
+    setBookingData({ ...bookingData, [e.target.name]: e.target.value });
+  };
+
+  const validateForm = () => {
+    const bookingValues = Object.values(bookingData);
+    const empty = (currentValue) => currentValue === '';
+    const bookingFieldValues = Object.values(bookingValues);
+    return !bookingFieldValues.some(empty);
+  };
+
+  const isFormValid = validateForm();
+
+  const submitForm = (formData) => {
+    const result = submitAPI(formData);
+    if (isFormValid && result) {
+      setBookingData({ ...initialFormValues });
+      navigate('/reservations/confirmation');
+    }
+    return;
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    submitForm(bookingData);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">       
-        <p>
-          Edit <code>src/App.js</code> and save to reload.!!!
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Nav />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about-us" element={<About />} />
+        <Route path="/reservations">
+          <Route
+            path="bookings"
+            element={
+              <BookingForm
+                times={availableTimes}
+                dispatch={handleDateChange}
+                handleSubmit={handleSubmit}
+                booking={bookingData}
+                handleBookingData={handleBookingData}
+                validateForm={validateForm}
+                // formik={formik}
+              />
+            }
+          />
+          <Route
+            path="details"
+            element={
+              <DetailsForm
+                booking={bookingData}
+                onSubmit={() => submitForm(bookingData)}
+                setData={setBookingData}
+                formik={formik}
+              />
+            }
+          />
+          <Route
+            path="/reservations/confirmation"
+            element={<ConfirmedBooking />}
+          />
+        </Route>
+        <Route path="/order-online" element={<Orders />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="*" element={<Error404 />} />
+      </Routes>
+      <Footer />
+    </>
   );
 }
 
-export default App;
+
--- little-lemon/src/App.test.js
+++ little-lemon/src/App.test.js
@@ -1,8 +1,198 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import { fireEvent, render, screen } from '@testing-library/react';
+import BookingForm from './components/BookingForm.js';
+import { fetchAPI, submitAPI } from './utils/api';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+describe('Booking Form', () => {
+  test('Renders the BookingForm heading', () => {
+    const times = ['17:00', '18:00', '19:00', '20:00', '21:00', '22:00'];
+
+    const dispatch = jest.fn();
+    const handleSubmit = jest.fn();
+    const handleBookingData = jest.fn();
+    const validateForm = jest.fn();
+    const booking = {
+      res_date: '',
+      res_time: '',
+      guests: '',
+      occasion: '',
+      seatingOption: '',
+    };
+
+    render(
+      <BookingForm
+        times={times}
+        dispatch={dispatch}
+        handleSubmit={handleSubmit}
+        booking={booking}
+        handleBookingData={handleBookingData}
+        validateForm={validateForm}
+      />
+    );
+
+    const heading = screen.getByText('Find a Table for Any Occasion');
+    expect(heading).toBeInTheDocument();
+  });
+
+  test('initializeTimes function returns the correct expected value', () => {
+    const times = ['17:00', '18:00', '19:00', '20:00', '21:00', '22:00'];
+
+    const initializeTimes = jest.fn(() => times);
+
+    expect(initializeTimes()).toBe(times);
+  });
+
+  // test('UpdateTimes function returns the same value that is provided in the state', () => {
+  //   const times = ['17:00', '18:00', '19:00', '20:00', '21:00', '22:00'];
+
+  //   const updateTimes = jest.fn(() => times);
+
+  //   expect(updateTimes()).toBe(times);
+  // });
+
+  test('UpdateTimes function returns different value than is provided in the state', () => {
+    const times = ['17:00', '18:00', '19:00', '20:00', '21:00', '22:00'];
+    const date = new Date('04/21/2023');
+    const updateTimes = jest.fn(() => fetchAPI(date));
+    const dispatch = jest.fn();
+    const handleSubmit = jest.fn();
+    const handleBookingData = jest.fn();
+    const validateForm = jest.fn();
+    const booking = {
+      res_date: '',
+      res_time: '',
+      guests: '',
+      occasion: '',
+      seatingOption: '',
+    };
+
+    render(
+      <BookingForm
+        times={times}
+        dispatch={dispatch}
+        handleSubmit={handleSubmit}
+        booking={booking}
+        handleBookingData={handleBookingData}
+        validateForm={validateForm}
+      />
+    );
+
+    const dateInput = screen.getAllByLabelText('*Date');
+    fireEvent.change(dateInput, { target: { value: date } });
+    expect(updateTimes()).not.toEqual(times);
+  });
+
+  test('Validate function returns true when all fields are filled', () => {
+    const bookingData = {
+      res_date: '04/21/2023',
+      res_time: '1700',
+      guests: '2',
+      occasion: 'Birthday',
+      seatingOption: 'Outside',
+    };
+    const validateForm = () => {
+      const bookingValues = Object.values(bookingData);
+      const empty = (currentValue) => currentValue === '';
+      const bookingFieldValues = Object.values(bookingValues);
+      return !bookingFieldValues.some(empty);
+    };
+
+    const validate = jest.fn(() => validateForm());
+    expect(validate()).toBe(true);
+  });
+  test('Validate function returns false when some or all fields are empty', () => {
+    const bookingData = {
+      res_date: '',
+      res_time: '',
+      guests: '',
+      occasion: 'Birthday',
+      seatingOption: 'Outside',
+    };
+
+    const validateForm = () => {
+      const bookingValues = Object.values(bookingData);
+      const empty = (currentValue) => currentValue === '';
+      const bookingFieldValues = Object.values(bookingValues);
+      return !bookingFieldValues.some(empty);
+    };
+    const validate = jest.fn(() => validateForm());
+    expect(validate()).toBe(false);
+  });
+
+  test('Form Submission Is Disabled When form fields are empty', () => {
+    const booking = {
+      res_date: '',
+      res_time: '',
+      guests: '',
+      occasion: '',
+      seatingOption: '',
+    };
+    const times = ['17:00', '18:00', '19:00', '20:00', '21:00', '22:00'];
+    const dispatch = jest.fn();
+    const handleSubmit = jest.fn();
+    const handleBookingData = jest.fn();
+    const validateForm = jest.fn();
+
+    render(
+      <BookingForm
+        times={times}
+        dispatch={dispatch}
+        handleSubmit={handleSubmit}
+        booking={booking}
+        handleBookingData={handleBookingData}
+        validateForm={validateForm}
+      />
+    );
+    const submitBtn = screen.getByRole('button');
+    fireEvent.click(submitBtn);
+    expect(handleSubmit).not.toHaveBeenCalled();
+    expect(validateForm).toHaveBeenCalled();
+    expect(submitBtn).toHaveHaveAttribute('disabled');
+  });
+
+  test('Form is successfully submited when all fields are filled', () => {
+    const booking = {
+      res_date: '',
+      res_time: '',
+      guests: '',
+      occasion: '',
+      seatingOption: '',
+    };
+    const date = new Date('04/21/2023');
+    const times = ['17:00', '18:00', '19:00', '20:00', '21:00', '22:00'];
+    const dispatch = jest.fn();
+    const handleSubmit = jest.fn();
+    const handleBookingData = jest.fn();
+    const validateForm = jest.fn();
+    render(
+      <BookingForm
+        times={times}
+        dispatch={dispatch}
+        handleSubmit={handleSubmit}
+        booking={booking}
+        handleBookingData={handleBookingData}
+        validateForm={validateForm}
+      />
+    );
+
+    const dateInput = screen.getByLabelText('*Date');
+    fireEvent.change(dateInput, { target: { value: date } });
+
+    const timeInput = screen.getByLabelText('*Choose time');
+    fireEvent.change(timeInput, { target: { value: '1700' } });
+
+    const guestsInput = screen.getByLabelText('*Number of Guests');
+    fireEvent.change(guestsInput, { target: { value: '4' } });
+
+    const occasionSelect = screen.getByLabelText('*Number of Guests');
+    fireEvent.change(occasionSelect, { target: { value: 'Birthday' } });
+
+    const radioInput = screen.getByLabelText('Outside');
+    fireEvent.change(radioInput, { target: { value: 'Outside' } });
+
+    const submitBtn = screen.getByRole('button');
+    fireEvent.click(submitBtn);
+    expect(handleSubmit).toHaveBeenCalled();
+    expect(validateForm).toHaveBeenCalled();
+    expect(submitBtn).not.toHaveHaveAttribute('disabled');
+  });
 });
