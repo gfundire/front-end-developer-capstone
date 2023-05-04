86ed09d907d1cf60e60438c12d72a9f29f2e32e3
Garikai <52905358+gfundire@users.noreply.github.com>
2023-05-04 14:01:12 +0000

    adds utils

---
+++ little-lemon/src/utils/siteLinks.js
@@ -0,0 +1,10 @@
+const siteLinks = [
+  { name: 'Home', path: '/' },
+  { name: 'About', path: '/about-us' },
+  { name: 'Reservations', path: '/reservations/bookings' },
+  { name: 'Confirmation', path: '/reservations/confirmation' },
+  { name: 'Order Online', path: '/reservations' },
+  { name: 'Login', path: '/login' },
+];
+
+export default siteLinks;
